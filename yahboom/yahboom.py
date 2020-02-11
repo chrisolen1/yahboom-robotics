a09fbd17bec945c97f8ea24af0378c4877f6c952
chrisolen1 <chrisolen1@gmail.com>
2020-02-11 13:21:42 -0600

    first attempt at orbit

--- yahboom/yahboom.py
+++ yahboom/yahboom.py
@@ -1,6 +1,7 @@
 import RPi.GPIO as GPIO
 import time
 import numpy as np
+import math
 
 class Yahboom():
 
@@ -14,7 +15,9 @@ class Yahboom():
             FRONT_SERVO = 23,
             CAMERA_SERVO_H = 25,
             CAMERA_SERVO_V = 9,
-            DEFAULT_FREQ = 1000):
+            DEFAULT_FREQ = 1000,
+            WHEEL_DIAMETER = 6.65,
+            WHEEL_BASE_WIDTH = 16):
         
         self.MOTOR_LEFT_FORWARD = MOTOR_LEFT_FORWARD
         self.MOTOR_LEFT_BACK = MOTOR_LEFT_BACK
@@ -26,6 +29,10 @@ class Yahboom():
         self.CAMERA_SERVO_H = CAMERA_SERVO_H
         self.CAMERA_SERVO_V = CAMERA_SERVO_V
         self.DEFAULT_FREQ = DEFAULT_FREQ
+        self.WHEEL_DIAMETER = WHEEL_DIAMETER # in cm
+        self.WHEEL_CIRCUMFERENCE = self.WHEEL_DIAMETER * math.pi
+        self.WHEEL_BASE_WIDTH = wheel_base_width # in cm
+        self.WHEEL_BASE_CIRCUMFERENCE = self.WHEEL_BASE_WIDTH * pi
     
         GPIO.setmode(GPIO.BCM)
         GPIO.setwarnings(False)
@@ -130,6 +137,87 @@ class Yahboom():
         GPIO.output(self.MOTOR_RIGHT_BACK, GPIO.LOW)
         pwm_MOTOR_LEFT_PWM.ChangeDutyCycle(left_percent)
         pwm_MOTOR_RIGHT_PWM.ChangeDutyCycle(right_percent)
+    
+    def orbit(self):
+
+        def orbit(self, speed, degrees, radius_cm=0, blocking=True):
+        
+        """
+        Drive in a circle  
+        
+        :param int degrees: Degrees to steer. **360** for full rotation. Negative for left turn.
+        :param int radius_cm: Radius in `cm` of the circle to drive. Default is **0** (turn in place).
+        :param boolean blocking = True: Set it as a blocking or non-blocking method.
+
+        """
+        assert (degrees <= 0 and degrees >= -360) or (degrees >= 0 and degrees <= 360), "invalid value for degrees"
+
+        radius = radius_cm * 10
+
+        # the total distance to drive in mm
+        drive_distance = math.pi * abs(radius) * abs(degrees) / 180 # / 180 is shorter than radius * 2 / 360
+        
+        # the distance in mm to add to one motor and subtract from the other
+        drive_difference = ((self.WHEEL_BASE_CIRCUMFERENCE * 10 * degrees) / 360)
+        
+        # the number of degrees each wheel needs to turn on average to get the necessary distance 
+        distance_degrees = ((drive_distance / self.WHEEL_CIRCUMFERENCE) * 360)
+        
+        # the difference between motor travel in degrees
+        difference_degrees = ((drive_difference / self.WHEEL_CIRCUMFERENCE) * 360)
+        
+        # the distance each wheel needs to turn
+        left_target  = (distance_degrees + difference_degrees)
+        right_target = (distance_degrees - difference_degrees)
+        
+        # if it's a left turn
+        if degrees < 0:
+            MOTOR_FAST = self.pwm_MOTOR_RIGHT_PWM
+            MOTOR_SLOW = self.pwm_MOTOR_LEFT_PWM
+            fast_target = right_target
+            slow_target = left_target
+        
+        # if it's a right turn
+        else:
+            MOTOR_FAST = self.pwm_MOTOR_LEFT_PWM
+            MOTOR_SLOW = self.pwm_MOTOR_RIGHT_PWM
+            fast_target = left_target
+            slow_target = right_target
+        
+        # determine driving direction from the speed
+        #direction = 1
+        #speed_with_direction = speed
+        #if speed < 0:
+        #    direction = -1
+        #    speed_with_direction = -speed
+        
+        # calculate the motor speed for each motor
+        fast_speed = speed
+        slow_speed = abs((speed * slow_target) / fast_target)
+        
+        # set the motor speeds
+        self.set_motor_limits(MOTOR_FAST, dps = fast_speed)
+        self.set_motor_limits(MOTOR_SLOW, dps = slow_speed)
+        
+        # get the starting position of each motor
+        StartPositionLeft = self.get_motor_encoder(self.MOTOR_LEFT)
+        StartPositionRight = self.get_motor_encoder(self.MOTOR_RIGHT)
+        
+        # Set each motor target position
+        self.set_motor_position(self.MOTOR_LEFT, (StartPositionLeft + (left_target * direction)))
+        self.set_motor_position(self.MOTOR_RIGHT, (StartPositionRight + (right_target * direction)))
+        
+        if blocking:
+            while self.target_reached(
+                    StartPositionLeft + (left_target * direction),
+                    StartPositionRight + (right_target * direction)) is False:
+                time.sleep(0.1)
+        
+            # reset to original speed once done
+            # if non-blocking, then the user is responsible in resetting the speed
+            self.set_speed(speed)
+        
+        return
     
     def stop(self):
         
